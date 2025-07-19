e1f0d3de012d62daf6990f6d144b177b07222ab0
Cosmas Musembi <musembicosmas254@gmail.com>
2025-07-19 13:18:15 -0700

    Fix: Match ALX output format for argument count

--- 0x12-javascript_warm_up/2-arguments.js
+++ 0x12-javascript_warm_up/2-arguments.js
@@ -1,13 +1,11 @@
 const args = process.argv.length - 2;
 
 if (args === 0) {
-  console.log('No argument');
+  console.log('0 argument');
 } else if (args === 1) {
-  console.log('Argument found');
+  console.log('1 argument');
 } else {
-  console.log('Arguments found');
+  console.log(`${args} arguments`);
 }
-node 0x12-javascript_warm_up/2-arguments.js
-node 0x12-javascript_warm_up/2-arguments.js Hello
-node 0x12-javascript_warm_up/2-arguments.js Hello World
+
 
